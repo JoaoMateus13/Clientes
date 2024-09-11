1a6528dbcaf84999d18e663551db0a2468cabf4d
João Mateus Silva <jms.lopes@discente.ufma.br>
2024-09-11 00:01:56 -0300

    Add Client entity with necessary fields and annotations

---
+++ src/main/java/com/home/crud_clientes/clientes/entities/Client.java
@@ -0,0 +1,30 @@
+package com.home.crud_clientes.clientes.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_client")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String cpf;
+    private Double income;
+    private LocalDate birthData;
+    private Integer children;
+
+}
