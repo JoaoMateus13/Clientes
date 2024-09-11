a2747c15e79b292d0a21e3e2bf66764f1e6f243f
jms-tre <joao.lopes@tre-ma.jus.br>
2024-09-11 19:18:42 -0300

    Add ClientController class for managing client endpoints

---
+++ src/main/java/com/home/crud_clientes/clientes/controllers/ClientController.java
@@ -0,0 +1,11 @@
+package com.home.crud_clientes.clientes.controllers;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientController {
+
+    
+
+}
