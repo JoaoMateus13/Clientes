c66cfabd108bd8667dd4e4041c099e6a5c1d3f17
jms-tre <joao.lopes@tre-ma.jus.br>
2024-09-11 19:18:02 -0300

    Add ClientRepository interface for managing client entities

---
+++ src/main/java/com/home/crud_clientes/clientes/repositories/ClientRepository.java
@@ -0,0 +1,8 @@
+
+
+package com.home.crud_clientes.clientes.repositories;
+import com.home.crud_clientes.clientes.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
