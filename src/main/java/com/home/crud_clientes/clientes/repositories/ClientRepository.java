a41bea5092d344a29cabf63457f4dface5c9fd11
jms-tre <joao.lopes@tre-ma.jus.br>
2024-09-12 18:07:26 -0300

    Add findByCpf method to ClientRepository

--- src/main/java/com/home/crud_clientes/clientes/repositories/ClientRepository.java
+++ src/main/java/com/home/crud_clientes/clientes/repositories/ClientRepository.java
@@ -5,4 +5,6 @@ import com.home.crud_clientes.clientes.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    Object findByCpf(String cpf);
 }
