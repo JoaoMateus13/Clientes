7106bc7662d66fc64808a787a0f326cb5a6d6c24
jms-tre <joao.lopes@tre-ma.jus.br>
2024-09-12 18:08:21 -0300

    Add findById, findAll, insert, update, and delete methods to ClientService

--- src/main/java/com/home/crud_clientes/clientes/services/ClientService.java
+++ src/main/java/com/home/crud_clientes/clientes/services/ClientService.java
@@ -1,19 +1,15 @@
 package com.home.crud_clientes.clientes.services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.home.crud_clientes.clientes.dto.ClientDTO;
 import com.home.crud_clientes.clientes.entities.Client;
 import com.home.crud_clientes.clientes.repositories.ClientRepository;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class ClientService {
@@ -21,14 +17,80 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
+    // Busca de recurso por id
     @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
-
         Client client = clientRepository.findById(id).get();
         return new ClientDTO(client);
     }
 
+    // Busca paginada de recursos
+    @Transactional(readOnly = true)
+    
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> listClient = clientRepository.findAll(pageable);
+        return listClient.map(x -> new ClientDTO(x));
+    }
 
+
+    // Inserir novo recurso.
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto){
+        /*Adicionar depois 
+
+        if(clientRepository.findByCpf(dto.getCpf()) != null){
+            throw new IllegalArgumentException("CPF já cadastrado");
+        }
+
+        */
+        Client client = new Client();
+        copyDtoToEntity(dto, client);
+        client = clientRepository.save(client);
+        return new ClientDTO(client);
+
+    }
+
+
+    // Atualizar recurso
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto){
+
+
+    //try{
+        Client client = clientRepository.getReferenceById(id);
+        copyDtoToEntity(dto, client);
+        client = clientRepository.save(client);
+        return new ClientDTO(client);
+    /*
+    }
+    catch (Exception e){
+        throw new IllegalArgumentException("Cliente não encontrado");
+    }
+        */
+        
+    }
+
+
+
+    // Deletar recurso
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+
+    public void delete(Long id){
+        clientRepository.deleteById(id);
+    }
+
+
+
+    private void copyDtoToEntity(ClientDTO dto, Client client) {
+        client.setName(dto.getName());
+        client.setCpf(dto.getCpf());
+        client.setIncome(dto.getIncome());
+        client.setBirthDate(dto.getBirthDate());
+        client.setChildren(dto.getChildren());
+    }
     
     
 
