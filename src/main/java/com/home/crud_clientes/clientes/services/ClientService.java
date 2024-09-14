f08a0f1c61c78fd7826549b4c86625f936d3f32f
joaomateus13 <jms.lopes2001@gmail.com>
2024-09-14 13:37:24 -0300

    Refactor ClientService to remove unused import

--- src/main/java/com/home/crud_clientes/clientes/services/ClientService.java
+++ src/main/java/com/home/crud_clientes/clientes/services/ClientService.java
@@ -1,6 +1,5 @@
 package com.home.crud_clientes.clientes.services;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
